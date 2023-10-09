0a4f66d8d897a2506f27012257544f957e5d4a52
Tanmay Binjola <145356457+Tanmay692004@users.noreply.github.com>
2023-10-10 03:20:03 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,17 @@
+
+    var typed = new Typed('#element', {
+      strings: ['Student.', 'Web Developer.'],
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+
+
+     function scrollToTop() {
+        window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling effect
+        });
+    }
+
